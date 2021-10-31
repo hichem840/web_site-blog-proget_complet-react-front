7d2676c67cfe589f509866f6df5c61538b5c2e10
hichem <kh_boulouza@esi.dz>
2021-10-31 16:45:04 +0100

    section

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
  <Navbar />
- <Home style={ {backgroundColor:"red"} }/>
+ <Home  />
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,25 @@
-
+import Background  from "./Background-image.png"
 const Home = () => {
     return (
-        <div clsseName="rie">
-        <body>
-            
-        </body>
+        <div clsseName="">
+          <section >
+              <div>
+         <h3 style={styleh3}>Tours&Travels</h3>
+         <h1> Amazing Place on Earth</h1>
+         <button>Exlore</button>
+         </div>
+          </section>
         </div>
     )
 }
 
+var styleh3={
+  
+   FontSize:"15px" ,
+   color:"white",
+   position:"absolute",
+
+}
 export default Home
+
+
