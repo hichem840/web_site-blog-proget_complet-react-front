7408bf30c1b3e7b21fe30114b37f590fc13bc2d4
hichem <kh_boulouza@esi.dz>
2021-11-04 23:56:33 +0100

    section1

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,18 @@ import Background  from "./Background-image.png"
 const Home = () => {
     return (
         <div clsseName="">
-          <section >
-            <div className=""> 
-             
-                    <h3 >Tours&Travels</h3>
-                    <h1 style={{color:"white"}}> Amazing Place on Earth</h1>
-                    <button >Exlore</button>
-            </div>
-          </section>
+                  <section >
+                        <div className="part1"> 
+                                <h3 >Tours&Travels</h3>
+                                <h1 style={{color:"white"}}> Amazing Place on Earth</h1>
+                                <button >Exlore</button>     
+                        </div>
+                            
+
+                    </section>
+                  
+                    
+                    
         </div>
     )
 }
